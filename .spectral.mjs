943a62f89791b9326de4719078a8eb039081115d
qa-vnxde-1-mdsj@example.com <qa-vnxde-1-mdsj@example.com>
2023-05-04 21:35:05 +0000

    First commit of promoted project

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://x-16359.stoplight-dev.com/api/v1/projects/cHJqOjQ3Ng/spectral.js?branch_slug=main&token=989b7024-0e3c-4422-bb54-7ccd9254ef1c";
+export default { extends: ruleset };
